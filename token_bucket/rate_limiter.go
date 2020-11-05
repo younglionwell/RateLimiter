77fbaad83f396811a4abdb31250ed168ce3869de
younglionwell <younglionwell@gmail.com>
2020-11-05 20:20:55 +0800

    add token bucket algorithm and its test code

--- token_bucket/rate_limiter.go
+++ token_bucket/rate_limiter.go
@@ -19,110 +19,111 @@
 ** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ****************************************************************************/
+// Package sliding_window_log provides a rate limiter implemented by
+// token bucket algorithm.
 
-// Package fixed_window_counter provides a rate limiter implemented by
-// sliding window counter algorithm.
-package sliding_window_counter
+package token_bucket
 
 import (
 	"context"
+	"encoding/json"
 	"math"
-	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
-// Limit defines the maximum frequency of some events.
-// Limit is represented as number of events per second.
-// A zero Limit allows no events.
 type Limit int64
 
-// Inf is the infinite rate limit; it allows all events.
 const Inf = Limit(math.MaxInt64)
 
 // RateLimiter controls how frequently events are allowed to happen.
-// The zero value is a valid Limiter, but it will reject all events.
 //
 // RateLimiter has two main method: Allow and Wait
 //
-// If the count of actionKey is greater or equal to the limit in
+// If the count of token in bucket is greater or equal to the 1 in
 // a period time, Allow returns false
 //
-// If the count of actionKey is greater or equal to the limit in
-// a period time, Wait blocks until  the count of actionKey is less than
-// the limit or its associated context.Context is canceled.
+// If the count of token in bucket is greater or equal to the limit in
+// a period time, Wait blocks until  the count of token in bucket is less
+// than the 1 or its associated context.Context is canceled.
 type RateLimiter struct {
 	actionKey string
-	// the action only happends limit times in a period time.
-	period   time.Duration
-	limit    Limit
-	// the interval time between two window counter
-	interval time.Duration
+	// Add a token into the bucket every period.
+	period time.Duration
+	// The size of token bucket
+	limit      Limit
+	expiration time.Duration
 
 	innerRedis *redis.Client
 }
 
-func (limiter *RateLimiter) getFiledKey(cur time.Time) string {
-	return strconv.FormatInt(cur.UnixNano() / limiter.interval.Nanoseconds(), 10)
+type tokenBucket struct {
+	TimeStamp int64 `json:"ts"`  // last update time
+	Count     int64 `json:"cnt"` // the sum of tokens in this bucket
 }
 
-// NewRateLimiter returns a new RateLimiter that allows the action up to happening
-// limit times in a period time.
-func NewRateLimiter(client *redis.Client, limit Limit, actionKey string, period time.Duration,
-	interval time.Duration) *RateLimiter {
+// NewRateLimiter returns a new RateLimiter that add a token into the bucket every period
+// and the size of its bucket is limit
+func NewRateLimiter(client *redis.Client, limit Limit, actionKey string, period time.Duration) *RateLimiter {
 	return &RateLimiter{
 		innerRedis: client,
 		limit:      limit,
 		actionKey:  actionKey,
 		period:     period,
-		interval:   interval,
+		expiration: period * time.Duration(limit) + time.Second, // avoid cold data
 	}
 }
 
-// Allow reports whether the action exceed the rate limit.
+// Allow is shorthand for AllowN(ctx, 1).
+func (limiter *RateLimiter) Allow(ctx context.Context) bool{
+	return limiter.AllowN(ctx, 1)
+}
+
+// AllowN reports whether n events may happen at time now.
 // Use this method if you intend to drop / skip action that exceed the rate limit.
 // Otherwise use Wait.
-func (limiter *RateLimiter) Allow(ctx context.Context) bool {
-	// remove expired counter
+func (limiter *RateLimiter) AllowN(ctx context.Context, n int64) bool {
 	// TODO: Should be an atomic operation. (Use LUA script maybe.)
-	result, err := limiter.innerRedis.HGetAll(ctx, limiter.actionKey).Result()
-	if err != nil { // something wrong with actionKey.
-		limiter.innerRedis.Del(ctx, limiter.actionKey)
-		return true
-	}
+	var bucket tokenBucket
+	result, err := limiter.innerRedis.Get(ctx, limiter.actionKey).Result()
 
-	// delete expire keys
-	timestamp := time.Now().UnixNano() / limiter.interval.Nanoseconds()
-	var delKey []string
-	wndSize := int64(0)
-	for k, v := range result {
-		ik, _ := strconv.ParseInt(k, 10, 64)
-		iv, _ := strconv.ParseInt(v, 10, 64)
-		if ik <= timestamp {
-			delKey = append(delKey, k)
-		} else {
-			wndSize += iv
+	// bucket is not full, check if it's enough for AllowN.
+	timestamp := time.Now().UnixNano() / limiter.period.Nanoseconds()
+	if err == nil && json.Unmarshal([]byte(result), &bucket) == nil {
+		// update bucket
+		bucket.Count += (timestamp - bucket.TimeStamp)
+		if Limit(bucket.Count) > limiter.limit {
+			bucket.Count = int64(limiter.limit)
 		}
+	} else {
+		// the bucket is full by default
+		bucket.Count = int64(limiter.limit)
 	}
-	limiter.innerRedis.HDel(ctx, limiter.actionKey, delKey...)
+	bucket.TimeStamp = timestamp
 
-	// check if up to limit
-	if Limit(wndSize) >= limiter.limit {
+	// token(s) is/are not enough.
+	if bucket.Count < n {
+		val, _ := json.Marshal(bucket)
+		limiter.innerRedis.SetEX(ctx, limiter.actionKey, val, limiter.expiration)
 		return false
 	}
 
-	// add for new action
-	limiter.innerRedis.HIncrBy(ctx, limiter.actionKey, limiter.getFiledKey(time.Now().Add(limiter.period)), 1)
-
-	// avoid cold data
-	limiter.innerRedis.Expire(ctx, limiter.actionKey, limiter.period+time.Second)
+	// get n tokens
+	bucket.Count -= n
+	val, _ := json.Marshal(bucket)
+	limiter.innerRedis.SetEX(ctx, limiter.actionKey, val, limiter.expiration)
 
 	return true
 }
 
-// Wait reports whether the action exceed the rate limit.
+// Wait is shorthand for WaitN(ctx, 1).
 func (limiter *RateLimiter) Wait(ctx context.Context) bool {
+	return limiter.WaitN(ctx, 1)
+}
+
+// WaitN blocks until lim permits n events to happen.
+func (limiter *RateLimiter) WaitN(ctx context.Context, n int64) bool {
 	// Check if ctx is already cancelled
 	select {
 	case <-ctx.Done():
@@ -131,12 +132,12 @@ func (limiter *RateLimiter) Wait(ctx context.Context) bool {
 	}
 
 	for {
-		if limiter.Allow(ctx) {
+		if limiter.AllowN(ctx, n) {
 			return true
 		}
 
 		// Allow failed, wait for limiter.interval
-		timer := time.NewTimer(limiter.interval)
+		timer := time.NewTimer(limiter.period)
 
 		select {
 		case <-timer.C:
